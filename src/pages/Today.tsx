57d3eb3daf2f07778d5cefc4f55389bb92833f08
roja-projects-only <github.rojaprojects@gmail.com>
2025-11-01 00:05:00 +0800

    chore(frontend): commit current changes

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -146,8 +146,8 @@ export function Today() {
 
           {/* Main Content - Two Column Layout */}
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            {/* Left Column - Today's Entries */}
-            <Card className="flex flex-col h-[600px] lg:h-[700px]">
+            {/* Left Column - Today's Entries */} 
+            <Card className="flex flex-col h-[600px] lg:h-[720px]">
               <CardHeader className="flex-shrink-0">
                 <CardTitle>Today's Entries</CardTitle>
               </CardHeader>
